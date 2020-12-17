229a057af9772d0801956681634ec648fc35281d
Your Name <you@example.com>
2020-12-17 13:16:31 +0800

    a

--- common/matric.py
+++ common/matric.py
@@ -13,5 +13,6 @@ import torch
 def evaluation(outputs, labels):
     outputs[outputs >= 0.5] = 1  # 大於等於0.5為有惡意
     outputs[outputs < 0.5] = 0  # 小於0.5為無惡意
-    correct = torch.mean(torch.eq(outputs, labels)).item()
-    return correct
+    correct = torch.sum(torch.eq(outputs, labels)).item()
+    acc = correct / len(labels) * 100
+    return acc
